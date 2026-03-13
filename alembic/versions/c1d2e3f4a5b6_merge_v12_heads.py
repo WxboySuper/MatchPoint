1240a35ad9c1e8fba4e2d4e0b89e7aa3b5e2fc8c
Alex Miller <alex.miller.6464@gmail.com>
2026-03-13 13:38:54 -0500

    style: modernize alembic merge migration typing

--- alembic/versions/c1d2e3f4a5b6_merge_v12_heads.py
+++ alembic/versions/c1d2e3f4a5b6_merge_v12_heads.py
@@ -5,10 +5,10 @@ Revises: 549c0b6c69a6, 6f9f9a8b7c6d
 Create Date: 2026-03-13
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 revision: str = "c1d2e3f4a5b6"
-down_revision: Union[str, Sequence[str], None] = (
+down_revision: str | Sequence[str] | None = (
     "549c0b6c69a6",
     "6f9f9a8b7c6d",
 )
